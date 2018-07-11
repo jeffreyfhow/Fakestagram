d4cdafb35fe1cde198b60fd8c2f66081eafb301b
Jeffrey How <jeffrey.f.how@gmail.com>
2018-07-10 17:33:36 -0700

    NetworkRequester Rx for comparison

--- app/src/main/java/com/jeffreyfhow/fakestagram/network/requester/NetworkRequester.java
+++ app/src/main/java/com/jeffreyfhow/fakestagram/network/requester/NetworkRequester.java
@@ -3,191 +3,88 @@ package com.jeffreyfhow.fakestagram.network.requester;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.jeffreyfhow.fakestagram.BuildConfig;
-import com.jeffreyfhow.fakestagram.mainactivity.MainActivity;
 import com.jeffreyfhow.fakestagram.data.Constants;
+import com.jeffreyfhow.fakestagram.mainactivity.MainActivity;
+import com.jeffreyfhow.fakestagram.data.Post;
 import com.jeffreyfhow.fakestagram.network.LogOutService;
 import com.jeffreyfhow.fakestagram.network.PostService;
-import com.jeffreyfhow.fakestagram.network.helper.GsonDeserializerManager;
-import com.jeffreyfhow.fakestagram.data.Post;
-import com.jeffreyfhow.fakestagram.network.helper.InterceptorFactory;
-import com.jeffreyfhow.fakestagram.network.helper.OkHttpClientManager;
-import com.jeffreyfhow.fakestagram.network.helper.RetrofitManager;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
+import hugo.weaving.DebugLog;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Call;
 
-public class NetworkRequester {
-
-    private MainActivity mainActivity;
-    private PostService postService;
-    private LogOutService logOutService;
+public class NetworkRequester extends NetworkRequesterBase<PostService, LogOutService> {
 
     public NetworkRequester(MainActivity mainActivity){
-        this.mainActivity = mainActivity;
-
-        // 1. Create Interceptors
-        Interceptor loggingInterceptor = InterceptorFactory.createHttpLoggingInterceptor(
-            HttpLoggingInterceptor.Level.BODY
-        );
-
-        HashMap<String, String> queryMap = new HashMap<>();
-        queryMap.put("response_type", Constants.RESPONSE_TYPE);
-        queryMap.put("client_id", Constants.CLIENT_ID);
-        Interceptor queryInterceptor = InterceptorFactory.createQueriesInterceptor(queryMap);
-
-        // 2. Create Interceptor Lists
-        ArrayList<Interceptor> loggingInterceptors = new ArrayList<>();
-        ArrayList<Interceptor> queryInterceptors = new ArrayList<>();
-        queryInterceptors.add(queryInterceptor);
-
-        if (BuildConfig.DEBUG) {
-            loggingInterceptors.add(loggingInterceptor);
-            queryInterceptors.add(loggingInterceptor);
-        }
-
-        // 3. Create HTTP Clients
-        OkHttpClient logOkHttpClient = OkHttpClientManager.getInstance().createOkHttpClient(
-            "logger", loggingInterceptors
-        );
-
-        OkHttpClient queryOkHttpClient = OkHttpClientManager.getInstance().createOkHttpClient(
-            "query", queryInterceptors
-        );
-
-        // 4. Create Shared RxJava2 Call Adapter
-        RxJava2CallAdapterFactory callAdapterFactory = RxJava2CallAdapterFactory.create();
-
-        // 5. Create Post Retrofit Client
-        Retrofit postRetrofit = RetrofitManager.getInstance().createRetrofit(
-            Constants.DATA_URL,
-            logOkHttpClient,
-            callAdapterFactory,
-            GsonConverterFactory.create(GsonDeserializerManager.getPostSearchGson())
-        );
-
-        Retrofit logOutRetrofit = RetrofitManager.getInstance().createRetrofit(
-            Constants.LOGIN_URL,
-            queryOkHttpClient,
-            callAdapterFactory,
-            GsonConverterFactory.create()
-        );
-
-        // 4. Create Web Services
-        postService = postRetrofit.create(PostService.class);
-        logOutService = logOutRetrofit.create(LogOutService.class);
+        super(mainActivity, PostService.class, LogOutService.class);
     }
 
-    public void sendGetPostsRequestRx(String id_token){
+    @DebugLog
+    @Override
+    public void sendGetPostsRequest(String id_token) {
         Observable<ArrayList<Post>> postsObservable = postService.getAllPosts(id_token);
 
         Disposable subscription = postsObservable
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                new Consumer<ArrayList<Post>>() {
-                    @Override
-                    public void accept(ArrayList<Post> posts) throws Exception {
-                        mainActivity.initializePosts(posts);
-                    }
-                },
-                new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        Toast.makeText(
-                            mainActivity,
-                            "Error retrieving posts. Please try again.",
-                            Toast.LENGTH_SHORT
-                        ).show();
-                        mainActivity.finish();
-                        Log.v("MainActivity", throwable.getMessage());
-                    }
-                },
-                new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        Log.d(
-                            NetworkRequester.this.getClass().getSimpleName(),
-                            "GetPosts request complete"
-                        );
-                    }
+                posts -> mainActivity.initializePosts(posts),
+                throwable -> {
+                    Toast.makeText(
+                        mainActivity,
+                        "Error retrieving posts. Please try again.",
+                        Toast.LENGTH_SHORT
+                    ).show();
+                    mainActivity.finish();
+                    Log.v("MainActivity", throwable.getMessage());
                 }
             );
     }
 
-//    public void sendGetPostsRequest(String id_token) {
-//        Call<ArrayList<Post>> call = postServiceOkHttp.getAllPosts("Bearer " + id_token);
-//        call.enqueue(new Callback<ArrayList<Post>>() {
-//            @Override
-//            public void onResponse(Call<ArrayList<Post>> call, Response<ArrayList<Post>> response) {
-//                mainActivity.initializePosts(response.body());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ArrayList<Post>> call, Throwable t) {
-//                Toast.makeText(mainActivity, "Error retrieving posts. Please try again.", Toast.LENGTH_SHORT).show();
-//                ActivityStarter.startAuthenticatorActivity(mainActivity);
-//                Log.v("MainActivity", t.getMessage());
-//            }
-//        });
-//    }
-//
-//    public void sendLikeRequest(String id_token, String id) {
-//        Call<Void> call = postServiceOkHttp.postLike("Bearer " + id_token, id);
-//        call.enqueue(new Callback<Void>() {
-//            @Override
-//            public void onResponse(Call<Void> call, Response<Void> response) {
-//                Toast.makeText(mainActivity, "Like Posted Response - Success.", Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Void> call, Throwable t) {
-//                Log.v("MainActivity", t.getMessage());
-//            }
-//        });
-//    }
-//
-//    public void sendUnlikeRequest(String id_token, String id) {
-//        Call<Void> call = postServiceOkHttp.deleteLike("Bearer " + id_token, id);
-//        call.enqueue(new Callback<Void>() {
-//            @Override
-//            public void onResponse(Call<Void> call, Response<Void> response) {
-//                Toast.makeText(mainActivity, "Like Deleted Response - Success", Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Void> call, Throwable t) {
-//                Log.v("MainActivity", t.getMessage());
-//            }
-//        });
-//    }
-//
-//    public void logOut(){
-//        Call<Void> call = logOutServiceOkHttp.logOut(Constants.LOGOUT_URL);
-//        call.enqueue(new Callback<Void>() {
-//            @Override
-//            public void onResponse(Call<Void> call, Response<Void> response) {
-//                ActivityStarter.startAuthenticatorActivity(mainActivity);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Void> call, Throwable t) {
-//                Log.d(this.getClass().getSimpleName(), t.getMessage());
-//                ActivityStarter.startAuthenticatorActivity(mainActivity);
-//            }
-//        });
-//    }
+    @DebugLog
+    @Override
+    public void sendLikeRequest(String id_token, String id) {
+        Observable<Void> likeObservable = postService.postLike("Bearer " + id_token, id);
+
+        Disposable subscription = likeObservable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                aVoid -> Toast.makeText(mainActivity, "Like Posted Response - Success.", Toast.LENGTH_SHORT).show(),
+                throwable -> Log.v("MainActivity", throwable.getMessage())
+            );
+    }
+
+    @DebugLog
+    @Override
+    public void sendUnlikeRequest(String id_token, String id) {
+        Observable<Void> unlikeObservable = postService.deleteLike("Bearer " + id_token, id);
+        Disposable subscription = unlikeObservable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                aVoid -> Toast.makeText(mainActivity, "Like Deleted Response - Success", Toast.LENGTH_SHORT).show(),
+                throwable -> Log.v("MainActivity", throwable.getMessage())
+            );
+    }
+
+    @DebugLog
+    @Override
+    public void logOut() {
+        Observable<Void> logOutObservable = logOutService.logOut(Constants.LOGOUT_URL);
+        Disposable subscription = logOutObservable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                aVoid -> mainActivity.finish(),
+                throwable -> mainActivity.finish()
+            );
+    }
 }
--- app/src/main/java/com/jeffreyfhow/fakestagram/network/requester/NetworkRequesterRxJava7.java
+++ app/src/main/java/com/jeffreyfhow/fakestagram/network/requester/NetworkRequesterRxJava7.java
@@ -3,6 +3,7 @@ package com.jeffreyfhow.fakestagram.network.requester;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.jeffreyfhow.fakestagram.data.Constants;
 import com.jeffreyfhow.fakestagram.mainactivity.MainActivity;
 import com.jeffreyfhow.fakestagram.data.Post;
 import com.jeffreyfhow.fakestagram.network.LogOutService;
@@ -10,18 +11,22 @@ import com.jeffreyfhow.fakestagram.network.PostService;
 
 import java.util.ArrayList;
 
+import hugo.weaving.DebugLog;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
+import retrofit2.Call;
 
 public class NetworkRequesterRxJava7 extends NetworkRequesterBase<PostService, LogOutService> {
+
     public NetworkRequesterRxJava7(MainActivity mainActivity){
         super(mainActivity, PostService.class, LogOutService.class);
     }
 
+    @DebugLog
     @Override
     public void sendGetPostsRequest(String id_token) {
         Observable<ArrayList<Post>> postsObservable = postService.getAllPosts(id_token);
@@ -60,18 +65,100 @@ public class NetworkRequesterRxJava7 extends NetworkRequesterBase<PostService, L
             );
     }
 
+    @DebugLog
     @Override
     public void sendLikeRequest(String id_token, String id) {
-        // TODO: 7/8/18  
+        Observable<Void> likeObservable = postService.postLike("Bearer " + id_token, id);
+
+        Disposable subscription = likeObservable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                new Consumer<Void>() {
+                    @Override
+                    public void accept(Void aVoid) throws Exception {
+                        Toast.makeText(mainActivity, "Like Posted Response - Success.", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.v("MainActivity", throwable.getMessage());
+                    }
+                },
+                new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        Log.d(
+                            this.getClass().getSimpleName(),
+                            "Like request complete"
+                        );
+                    }
+                }
+            );
     }
 
+    @DebugLog
     @Override
     public void sendUnlikeRequest(String id_token, String id) {
-        // TODO: 7/8/18  
+        Observable<Void> unlikeObservable = postService.deleteLike("Bearer " + id_token, id);
+        Disposable subscription = unlikeObservable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                new Consumer<Void>() {
+                    @Override
+                    public void accept(Void aVoid) throws Exception {
+                        Toast.makeText(mainActivity, "Like Deleted Response - Success", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.v("MainActivity", throwable.getMessage());
+                    }
+                },
+                new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        Log.d(
+                            this.getClass().getSimpleName(),
+                            "Unlike request complete"
+                        );
+                    }
+                }
+            );
     }
 
+    @DebugLog
     @Override
     public void logOut() {
-        // TODO: 7/8/18  
+        Observable<Void> logOutObservable = logOutService.logOut(Constants.LOGOUT_URL);
+        Disposable subscription = logOutObservable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                new Consumer<Void>() {
+                    @Override
+                    public void accept(Void aVoid) throws Exception {
+                        mainActivity.finish();
+                    }
+                },
+                new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        mainActivity.finish();
+                    }
+                },
+                new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        Log.d(
+                            this.getClass().getSimpleName(),
+                            "Log Out request complete"
+                        );
+                    }
+                }
+            );
     }
 }
